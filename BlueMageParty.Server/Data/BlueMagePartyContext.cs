65fd628bf01a4bf7ad93b0a28a7bb80e7bab4543
joeaculberson <Joeaculberson@gmail.com>
2024-11-19 18:25:38 -0500

    Added Entity Framework

---
+++ BlueMageParty.Server/Data/BlueMagePartyContext.cs
@@ -0,0 +1,19 @@
+﻿using BlueMageParty.Server.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlueMageParty.Server.Data
+{
+    public class BlueMagePartyContext : DbContext
+    {
+        public DbSet<User> Users { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            .AddJsonFile("appsettings.json")
+            .Build();
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("BlueMagePartyContext"));
+        }
+    }
+}
---
+++ BlueMageParty.Server/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace BlueMageParty.Server.Models
+{
+    public class User
+    {
+        public int Id { get; set;  }
+        public String UserName { get; set; } = null!;
+        public String Password { get; set; } = null!;
+        public string Email { get; set; } = null!;
+    }
+}
