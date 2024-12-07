65e67914d72a94ab61c11f09192052747091db58
joeaculberson <Joeaculberson@gmail.com>
2024-12-06 23:19:29 -0500

    Added a nav bar

--- bluemageparty.client/src/pages/index.vue
+++ bluemageparty.client/src/pages/index.vue
@@ -1,5 +1,11 @@
 <template>
-  <HelloWorld />
+  <v-app-bar :elevation="2">
+    <template v-slot:prepend>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    </template>
+
+    <v-app-bar-title>Blue Mage Party</v-app-bar-title>
+  </v-app-bar>
 </template>
 
 <script lang="ts" setup>
