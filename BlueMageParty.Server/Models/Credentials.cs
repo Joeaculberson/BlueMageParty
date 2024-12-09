d751cc922b046d31d192390a6ea8d7ef42bb9368
joeaculberson <Joeaculberson@gmail.com>
2024-12-09 10:20:33 -0500

    Updating verbiage

--- BlueMageParty.Server/Models/Credentials.cs
+++ BlueMageParty.Server/Models/Credentials.cs
@@ -1,9 +1,10 @@
 ﻿namespace BlueMageParty.Server.Models
 {
+
     public class Credentials
     {
         /// <summary>
-        /// If you want to run this project on your, you'll need to generate your own 256-bit/512-bit key.
+        /// If you want to run this project on your own, you'll need to generate your own 256-bit/512-bit key.
         /// To generate your own key, go to https://randomkeygen.com/ and copy/paste the 256-bit/512-bit WEP Keys option to the const below.
         /// </summary>
         public const string SymmetricSecurityKey = null;
