ca148abf8ec449b93f0673f2698053504caee157
joeaculberson <Joeaculberson@gmail.com>
2024-12-10 17:34:29 -0500

    Updating Credentials language Part 4.

--- BlueMageParty.Server/Models/Credentials.cs
+++ BlueMageParty.Server/Models/Credentials.cs
@@ -1,12 +1,13 @@
 ﻿namespace BlueMageParty.Server.Models
 {
-
     public class Credentials
     {
         /// <summary>
-        /// If you want to run this project on your own, you'll need to generate your own base64 key.
-        /// To generate your own key, go to https://generate.plus/en/base64 and copy/paste the base64 key below.
+        /// If you want to run this project on your own, you'll need to generate your own key.
+        /// To generate your own key, run this command: "openssl rand -base64 64".
+        /// Windows Users: If you have git installed you can find openssl here: C:\Program Files\Git\usr\bin
+        /// then copy/paste the key into the string below in to JWTSecurityKey.
         /// </summary>
-        public const string SymmetricSecurityKey = "";
+        public const string JWTSecurityKey = "";
     }
 }
