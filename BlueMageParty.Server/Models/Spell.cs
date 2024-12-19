33555fb6535be76403d373add241648b384e33ec
joeaculberson <Joeaculberson@gmail.com>
2024-12-19 14:28:48 -0500

    Added spells class and migration

---
+++ BlueMageParty.Server/Models/Spell.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlueMageParty.Server.Models
+{
+    public class Spell
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public int Number { get; set; }
+        public string Enemy { get; set; } = null!;
+        public string Location { get; set; } = null!;
+        public string Patch { get; set; } = null!;
+        public string Icon { get; set; } = null!;
+    }
+}
