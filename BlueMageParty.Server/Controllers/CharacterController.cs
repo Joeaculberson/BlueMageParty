83f65e1435d53d159bf633aaed2bfe312d0b26f2
joeaculberson <Joeaculberson@gmail.com>
2025-01-09 18:16:01 -0500

    Added character search functionality

---
+++ BlueMageParty.Server/Controllers/CharacterController.cs
@@ -0,0 +1,17 @@
+﻿using BlueMageParty.Server.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlueMageParty.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CharacterController : ControllerBase
+    {
+        private readonly BlueMagePartyContext _context;
+
+        public CharacterController(BlueMagePartyContext context)
+        {
+            _context = context;
+        }
+    }
+}
