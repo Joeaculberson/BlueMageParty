c26593f7df46b8394d0b66d06e08a02c08ffac1d
joeaculberson <Joeaculberson@gmail.com>
2025-03-18 12:53:57 -0400

    Adding test connection class

---
+++ BlueMageParty.Server/TestConnection.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace BlueMageParty.Server.TestConnection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = Environment.GetEnvironmentVariable("ConnectionStrings__BlueMagePartyContext");
+            Console.WriteLine("Testing connection to database...");
+
+            try
+            {
+                using (var connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    Console.WriteLine("Connection successful!");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Connection failed: {ex.Message}");
+            }
+        }
+    }
+}
